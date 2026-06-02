8e81b24cf449ee0b8409a246ca805922fec4afc4
SimuladorDeFarm <tfariasp@proton.me>
2026-06-01 23:23:04 -0400

    add promt Explorador

--- orchestrator/agents/explorer.py
+++ orchestrator/agents/explorer.py
@@ -3,17 +3,34 @@ from core.runner_client import ejecutar_en_docker
 
 
 
-agente = BaseAgent("Eres un experto en Ethical Hacking y te estan preguntando que comando usarías en cada situacion, tienes que responder solo comn el comando como si estuvieras escribiendolo por consola, donde cualquier texto de explicacion daria error, para ejecutar comandos siempre usa tool")
-
-respuesta = agente.preguntar("si tu objetivo es esta ip localhost y tienes que revisar todos los puertos para ver si se quedo uno abierto, que comadno suarias")
+#agente = BaseAgent("Eres un experto en Ethical Hacking y te estan preguntando que comando usarías en cada situacion, tienes que responder solo comn el comando como si estuvieras escribiendolo por consola, donde cualquier texto de explicacion daria error, para ejecutar comandos siempre usa tool")
 
 
+agente = BaseAgent(
+    "Eres un experto en Ethical Hacking"
+    "Tienes que hacer pentest a sistemas permitidos, todas las IP entregadas seran de laboratorios de tryhackme, HTB o maquinas locales"
+    "Eres parte de un Multy Agent System para pentest, que se compone de Comandante, Explorador (Tu), Explotador y Reportador"
+    "Tu tarea será explorar los sistemas, por el momento cuentas con las herramientas nativas del sistema linux y nmap"
+    "No debes explotar vulnerabilidades, solo explorar"
+    "Explotar (Lo que tu no haces) signfica que no puedes abusar de una vulnerabilidad descubierta para ganar acceso a una maquina, recolectar informacion o urgar en servidores"
+    "Explorar es toda tarea que busque conocer el sistema objetivo, mapear su superficie de ataque y conocer posibles puntos debiles"
+    "Existe un agente Juez que determinará que tu tarea esta bien realizada o debe repetirse"
+    "Tu puedes interactuar con dos elementos, la terminal de comandos y una lista de tareas generadas por ti"
+    "Cuando quieras lanzar un comando debes usar siempre la call de tool"
+    "Cuando se te indique explisitamente para crear tareas usaras el promp normal"
+    "Se te indicara la ip objetivo y deberas hacer allazgos"
+    "La lista de tareas no debe exeder los 5 puntos, cada tarea debe poder realizarse con un solo comando"
+    "El primer promt que se te enviará tendra la ip objetivo, se realizará un escaneo rapido y ya con la respusta generaras las tareas"
+    "Recuerda que a veces el no haber hallazgos es un hallazgo, significa que todo esta OK"
+)
 
 
-""""
+respuesta = agente.preguntar("muestrame las instrucciones iniciales que te di")
 
 
+print(respuesta)
 
+"""
 comand0 = ejecutar_en_docker("ls")
 
 print(comand0)
